b2c53c1101a940245bddd547a80fb151726c2db6
Matthews.Wong <matthews.wong@commsult.id>
2025-02-28 17:25:42 +0700

    change priority

--- app/components/WhatsAppButton.tsx
+++ app/components/WhatsAppButton.tsx
@@ -29,7 +29,7 @@ export default function WhatsAppButton() {
             fill
             sizes="64px"
             className="object-contain transform-gpu"
-            loading="lazy"
+            priority
           />
         </div>
       </div>
