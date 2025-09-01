84f5abe554724847b8bbeb3c7aec2517b4694c4d
Matthews.Wong <matthews.wong@commsult.id>
2025-09-01 18:22:38 +0700

    disable unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // ❌ turn off unused var error completely
+      // "@typescript-eslint/no-unused-vars": "off",
+
+      // ✅ or, better: only allow unused vars if prefixed with _
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
+      ]
+    },
+  },
 ];
 
 export default eslintConfig;
